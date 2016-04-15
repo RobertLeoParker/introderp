d0ab80470cabb9804e1eceb1c92888bb65757815
Robert Parker <Robert.leo.parker@gmail.com>
2016-04-15 12:39:50 -0500

    Initial Commit

---
+++ IntroDerp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  IntroDerp
+//
+//  Created by Robert Parker on 4/15/16.
+//  Copyright © 2016 Robert Parker. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
